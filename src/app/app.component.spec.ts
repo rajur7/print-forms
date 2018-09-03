b47745a1095f1ec9da0a23de0bd5e50a0ae4f8a3
Prasanna <lakshmip@thoughtworks.com>
2018-09-03 15:24:26 +0530

    Alekhya, Prasanna | #1 | Make Openmrs root url as dynamic host url

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, getTestBed, TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-  public static OPENMRS_ROOT_URL = 'https://192.168.33.10/openmrs/ws/rest/v1/';
+  public static OPENMRS_ROOT_URL = 'https://' + window.location.hostname + '/openmrs/ws/rest/v1/';
   public static PRINT_FORMS_PRIVILEGE = 'app:print-forms';
   public static NO_PRIVILEGE_ERROR = 'User is logged in but doesn\'t have the relevant privilege';
 }
