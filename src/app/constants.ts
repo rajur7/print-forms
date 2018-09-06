d676fea7c43478f8206f7c90842dbfc6342947db
Prasanna <lakshmip@thoughtworks.com>
2018-09-06 12:22:11 +0530

    Prasanna | #15 | Remove locale from request url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -3,7 +3,7 @@ export class Constants {
   public static PRINT_FORMS_PRIVILEGE = 'app:print-forms';
   public static NO_PRIVILEGE_ERROR = 'User is logged in but doesn\'t have the relevant privilege';
   public static WHOAMI_URL = 'bahmnicore/whoami';
-  public static ALL_OBSERVATION_TEMPLATES_URL = 'concept?s=byFullySpecifiedName&locale=en&name=All+Observation+Templates&v' +
+  public static ALL_OBSERVATION_TEMPLATES_URL = 'concept?s=byFullySpecifiedName&name=All+Observation+Templates&v' +
     '=custom:(setMembers:(display))';
 }
 
